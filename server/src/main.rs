b106709014c4408442b84be1fec2acc2ebc638ac
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2021-12-17 00:30:16 +0000

    async main

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,8 @@ mod mongo;
 
 use crate::mongo::*;
 
-fn main() {
-    let c = get_palavras_collection();
+#[tokio::main]
+async fn main() {
+    let c = get_palavras_collection().await.unwrap();
+    println!("{:#?}", c);
 }
